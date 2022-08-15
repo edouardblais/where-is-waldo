985d37dfce81ca31a0485b9da780195e6cae4edc
edouard <edouard.blais@hotmail.com>
2022-08-15 14:34:41 -0400

    implemented photo tagging modal

--- src/components/playBeach.js
+++ src/components/playBeach.js
@@ -23,6 +23,16 @@ const playBeach = () => {
     time.starttimer();
   });
 
+  const tagmodal = document.createElement('div');
+  tagmodal.classList.add('tagmodal');
+  beach.addEventListener('click', (e) => {
+    const xcoord = e.clientX - 20;
+    const ycoord = e.clientY - 20;
+    tagmodal.style.left = `${xcoord}px`;
+    tagmodal.style.top = `${ycoord}px`;
+    tagmodal.classList.add('showmodal');
+  });
+
   const backtohomebutton = document.getElementById('backtohomebutton');
   backtohomebutton.addEventListener('click', () => {
     time.stoptimer();
@@ -30,6 +40,7 @@ const playBeach = () => {
 
   playcontainer.appendChild(beach);
   playcontainer.appendChild(startmodal);
+  playcontainer.appendChild(tagmodal);
 
   content.appendChild(playcontainer);
 };
